c2ef2b9d708b1383ec984927fe9614765e3f763f
timursha <timvlpn@gmail.com>
2018-07-13 14:44:33 +0300

    `2`

---
+++ src/app/first/components/confirmation/confirmation.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation',
+  templateUrl: 'confirmation.component.html',
+  styleUrls: ['confirmation.component.css'],
+})
+export class ConfirmationComponent {}
