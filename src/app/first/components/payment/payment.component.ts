023f5ee75da6e408ca06b3e98f9e093994b843c4
valeriy <v.gerasimov@innopoli.ru>
2018-07-18 08:36:34 +0300

    adding functions

---
+++ src/app/first/components/payment/payment.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {switchMap} from 'rxjs/internal/operators';
+import {Order} from '../../models/Order';
+import {OrderService} from '../../services/order.service';
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.css']
+})
+export class PaymentComponent implements OnInit {
+  order: Order;
+
+  constructor(private route: ActivatedRoute,
+              private orderService: OrderService,) { }
+
+
+  ngOnInit() {
+    this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.orderService.getOrder(params.get('id')))
+    ).subscribe((order: Order) => this.order = order);
+  }
+
+}
