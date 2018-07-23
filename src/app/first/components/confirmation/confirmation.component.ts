678eb896091a44bcedb666ea2944152b6dd6c3b3
valeriy <v.gerasimov@innopoli.ru>
2018-07-24 00:08:29 +0300

    debug

--- src/app/first/components/confirmation/confirmation.component.ts
+++ src/app/first/components/confirmation/confirmation.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+import {BasketService} from '../../services/basket.service';
+import {switchMap} from 'rxjs/internal/operators';
+import {Basket} from '../../models/Basket';
 
 @Component({
   selector: 'app-confirmation',
@@ -6,7 +10,15 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['confirmation.component.css'],
 })
 export class ConfirmationComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) { }
+  id: string;
+
   ngOnInit(): void {
+    this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.id = params.get('id')
+      )
+    );
   }
 
 }
