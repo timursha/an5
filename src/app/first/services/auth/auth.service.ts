d01f48e2311716a6b9efb5052cb42ef4018a162e
valeriy <v.gerasimov@innopoli.ru>
2018-07-31 12:40:54 +0300

    adding design to login page

--- src/app/first/services/auth/auth.service.ts
+++ src/app/first/services/auth/auth.service.ts
@@ -21,23 +21,25 @@ export class AuthService {
 
   public login(user: User): Observable<any> {
     return this.httpClient.post<any>('api/api-token-auth/', {'username': user.username, 'password': user.password}).pipe(
-      map((data: any) => {
-          console.log(`This is service data ${data}`);
-          if (data.status === 200 || data.status === 201) {
-            localStorage.setItem('token', data.token);
-            this.location.back();
-            return of(null);
-          } else {
-            if (data.status === 400) {
-              return of(data.error);
-            }
-          }
-          return of(null);
-
-        }),
       catchError(err => {
-        console.log(`This is error ${err}`);
+        console.log(`This is error`);
+        console.log(err);
         return of(err);
+      }),
+      map((data: any) => {
+        console.log(`This is service data`);
+        console.log(data);
+        if (data.status === 200 || data.status === 201) {
+          localStorage.setItem('token', data.token);
+          this.location.back();
+          return of(null);
+        } else {
+          if (data.status === 400) {
+            return of(data.error);
+          }
+        }
+        return of(null);
+
       })
     );
 
